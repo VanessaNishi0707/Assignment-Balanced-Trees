76323c9566ecde2b1fdcbf9fb7967018f5672219
Vanessa <vanessa.nishi2021@gmail.com>
2024-09-21 13:55:10 -1000

    test

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // Required for input and output
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number" << endl;
+    cin >> num;
+
+    if (num > 10) {
+        cout << "That's a big number"; 
+    }
+
+    return 0;
+}
