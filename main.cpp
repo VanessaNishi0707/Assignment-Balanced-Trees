9929d05bc25ffab28b39cf5c06eabf8caa81462f
Vanessa Nishi <vanessa.nishi2021@gmail.com>
2024-09-23 00:56:32 -1000

    f

--- main.cpp
+++ main.cpp
@@ -1,14 +1,40 @@
 #include <iostream> // Required for input and output
 using namespace std;
+#include <string>
+#include <stdio.h>
+#include <io.h>
+#include <fcntl.h>
+
+
+enum Color {
+    RED,
+    BLACK
+};
+
+struct Saying {
+    wstring hawaiian[2]; // [0] is key and saying in Hawaiian. [1] is meaning in Hawaiian
+    wstring english[2]; // [0] is saying in English. [1] is saying in English
+
+    Color color;
+
+    Saying *left;
+    Saying *right;
+    Saying *parent;
+};
+
+class Dictionary {
+    public:
+        Saying saying;
+
+};
 
 int main() {
-    int num;
-    cout << "Enter a number" << endl;
-    cin >> num;
+    Dictionary dictionary;
 
-    if (num > 10) {
-        cout << "That's a big number"; 
-    }
+    wchar_t waname[] = L"Aia a pa'i 'ia ka maka, ha'i 'ia kupuna nāna 'oe" ;
+    char16_t* text2;
+
+    wcout << waname << endl;
 
     return 0;
 }
